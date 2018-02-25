84d4b67b9b04967d97ab7c971bb9343b1c2d710c
Adwi Arifin <adwiarifin@gmail.com>
2018-02-25 21:20:48 +0700

    Change form name style to Title Case Style

--- src/com/maskiproj/form/Formula.java
+++ src/com/maskiproj/form/Formula.java
@@ -9,12 +9,12 @@ package com.maskiproj.form;
  *
  * @author fauzi
  */
-public class formula extends javax.swing.JFrame {
+public class Formula extends javax.swing.JFrame {
 
     /**
      * Creates new form formula
      */
-    public formula() {
+    public Formula() {
         initComponents();
     }
 
@@ -194,20 +194,21 @@ public class formula extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(formula.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Formula.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(formula.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Formula.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(formula.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Formula.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(formula.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Formula.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new formula().setVisible(true);
+                new Formula().setVisible(true);
             }
         });
     }
--- src/com/maskiproj/form/Kalkulator.java
+++ src/com/maskiproj/form/Kalkulator.java
@@ -9,12 +9,12 @@ package com.maskiproj.form;
  * @author fauzi
  * afnan13579@gmail.com
  */
-public class kalkulator extends javax.swing.JFrame {
+public class Kalkulator extends javax.swing.JFrame {
 
     /**
      * Creates new form kalkulator
      */
-    public kalkulator() {
+    public Kalkulator() {
         initComponents();
     }
 
@@ -402,21 +402,23 @@ public class kalkulator extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(kalkulator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Kalkulator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(kalkulator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Kalkulator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(kalkulator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Kalkulator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(kalkulator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Kalkulator.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new kalkulator().setVisible(true);
+                new Kalkulator().setVisible(true);
             }
         });
     }
