0f9495b260c967f0d330c480faf26e50b0bdf7b7
Adwi Arifin <adwiarifin@gmail.com>
2018-02-25 21:38:04 +0700

    Remove main function in each Form, then create new Main as a single router

--- src/com/maskiproj/form/Laporan.java
+++ src/com/maskiproj/form/Laporan.java
@@ -1,20 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.maskiproj.form;
 
+import com.maskiproj.main.Main;
+
 /**
- *
  * @author fauzi
+ * afnan13579@gmail.com
  */
 public class Laporan extends javax.swing.JFrame {
 
+    private final Main main;
+    
     /**
      * Creates new form Laporan
      */
-    public Laporan() {
+    public Laporan(Main main) {
+        this.main = main;
+        
         initComponents();
     }
 
@@ -111,41 +112,6 @@ public class Laporan extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Laporan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Laporan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Laporan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Laporan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Laporan().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
---
+++ src/com/maskiproj/main/Main.java
@@ -0,0 +1,87 @@
+package com.maskiproj.main;
+
+import com.maskiproj.form.Formula;
+import com.maskiproj.form.Kalkulator;
+import com.maskiproj.form.Laporan;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Sasuke
+ */
+public class Main {
+    
+    private final Formula fFormula;
+    private final Kalkulator fKalkulator;
+    private final Laporan fLaporan;
+    
+    private final Connection conn;
+
+    public Main() {
+        this.conn = getConnection();
+        
+        fFormula = new Formula(this);
+        fKalkulator = new Kalkulator(this);
+        fLaporan = new Laporan(this);
+    }
+    
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            Main main = new Main();
+            
+            main.showFormula();
+        });
+    }
+    
+    public void showFormula() {
+        fFormula.setVisible(true);
+    }
+    
+    public void hideFormula() {
+        fFormula.setVisible(false);
+    }
+    
+    public void showKalkulator() {
+        fKalkulator.setVisible(true);
+    }
+    
+    public void hideKalkulator() {
+        fKalkulator.setVisible(false);
+    }
+    
+    public void showLaporan() {
+        fLaporan.setVisible(true);
+    }
+    
+    public void hideLaporan() {
+        fLaporan.setVisible(false);
+    }
+    
+    private Connection getConnection() {
+        String server = "localhost";
+        String database = "tokoanu";
+        String username = "root";
+        String password = "";
+        String host = "jdbc:mysql://" + server + "/" + database;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection koneksi = DriverManager.getConnection(host, username, password);
+            return koneksi;
+        } catch (ClassNotFoundException | SQLException ex) {
+            // show error
+            JOptionPane.showMessageDialog(null, "Tidak dapat menyambung ke database", "Koneksi gagal", JOptionPane.ERROR_MESSAGE);
+            exit();
+        }
+
+        return null;
+    }
+    
+    public void exit() {
+        System.exit(0);
+    }
+    
+}
