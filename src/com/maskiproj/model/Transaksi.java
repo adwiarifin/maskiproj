7e32f529c6b743b13fd2bf528c2eadb6fc33b576
Adwi Arifin <adwiarifin@gmail.com>
2018-03-01 12:43:59 +0700

    Change Double.parseDouble to NumberFormat.parse in Transaksi

--- src/com/maskiproj/model/Transaksi.java
+++ src/com/maskiproj/model/Transaksi.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,14 +14,14 @@ import java.util.logging.Logger;
  * @author Adwi Arifin <adwiarifin@gmail.com>
  */
 public class Transaksi {
-    
+
     public static final String[] TRANSAKSI_COLUMN_TITLE = {"Tanggal", "Pemesan", "Jenis Pesanan", "Total"};
     private final Connection conn;
 
     public Transaksi(Connection conn) {
         this.conn = conn;
     }
-    
+
     public String[][] getListTransaksi() {
         String[][] result = null;
         try {
@@ -46,7 +48,7 @@ public class Transaksi {
         }
         return result;
     }
-    
+
     public String[][] getListTransaksiForCustomer() {
         String[][] result = null;
         try {
@@ -73,13 +75,13 @@ public class Transaksi {
         }
         return result;
     }
-    
-    public boolean insertTransaksi(String pemesan, String jenisPesanan, double total, String[][] detailTransaksi){
+
+    public boolean insertTransaksi(String pemesan, String jenisPesanan, double total, String[][] detailTransaksi) {
         boolean result = false;
         try {
             // start transaction
             conn.setAutoCommit(false);
-            
+
             // insert to transaksi
             String sqlTransaksi = "INSERT INTO transaksi(pemesan, jenis_pesanan, total, tanggal) VALUES(?, ?, ?, NOW())";
             PreparedStatement stmtTransaksi = conn.prepareStatement(sqlTransaksi, PreparedStatement.RETURN_GENERATED_KEYS);
@@ -87,37 +89,47 @@ public class Transaksi {
             stmtTransaksi.setString(2, jenisPesanan);
             stmtTransaksi.setDouble(3, total);
             stmtTransaksi.executeUpdate();
-            
+
             // get transaksi insert id
             int transaksiId = 0;
             ResultSet rs = stmtTransaksi.getGeneratedKeys();
             if (rs.next()) {
                 transaksiId = rs.getInt(1);
             }
-            
+
             // insert to detail_transaksi
             for (String[] column : detailTransaksi) {
                 String material = column[0];
-                double luas = Double.parseDouble(column[1]);
-                double hasil = Double.parseDouble(column[2]);
-                double subtotal = Double.parseDouble(column[3]);
                 
+                // get double value
+                NumberFormat format = NumberFormat.getInstance();
+                double luas = 0;
+                double hasil = 0;
+                double subtotal = 0;
+                try {
+                    luas = format.parse(column[1]).doubleValue();
+                    hasil = format.parse(column[2]).doubleValue();
+                    subtotal = format.parse(column[3]).doubleValue();
+                } catch (ParseException ex) {
+                    conn.rollback();
+                }
+
                 String sqlDetailTransaksi = "INSERT INTO detail_transaksi(id_transaksi, material, luas, hasil, total) VALUES(?, ?, ?, ?, ?)";
                 PreparedStatement stmtDetailTransaksi = conn.prepareStatement(sqlDetailTransaksi);
-                
+
                 stmtDetailTransaksi.setInt(1, transaksiId);
                 stmtDetailTransaksi.setString(2, material);
                 stmtDetailTransaksi.setDouble(3, luas);
                 stmtDetailTransaksi.setDouble(4, hasil);
                 stmtDetailTransaksi.setDouble(5, subtotal);
-                
+
                 stmtDetailTransaksi.executeUpdate();
             }
-            
+
             // end transaction
             conn.commit();
             conn.setAutoCommit(true);
-            
+
             // change return value
             result = true;
         } catch (SQLException ex) {
